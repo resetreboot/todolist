c6c6cb0a6ad7c90913b7f4cb2836ef5fc50fdebf
José Carlos Cuevas <reset.reboot@gmail.com>
2015-02-25 00:37:38 +0100

    Added GPL headers where needed, verified it and added a 512x512 icon too

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+/*  This file is part of ToDo List.
+
+    ToDo List is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ToDo List is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ToDo List. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // DOMContentLoaded is fired once the document has been loaded and parsed,
 // but without waiting for other external resources to load (css/images/etc)
 // That makes the app more responsive and perceived as faster.
